99ca1441b92f8b07bfb2727de0953a7a1033c05e
RishabhYadav-Devloper <rishabh18yad@gmail.com>
2025-04-21 01:03:55 +0530

    Update Exp-6.c

--- Exp-6.c
+++ Exp-6.c
@@ -50,3 +50,46 @@ break;
 } 
 return 0; 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Title: To Implement and analyze concepts of synchronization and deadlocks.
+// Aim: Write a C program to implement solution of Producer consumer problem through
+// Semaphore
+// Theory:
+// What is the Producer-Consumer Problem?
+// The producer-consumer problem is an example of a multi-process synchronization problem. The
+// problem describes two processes, the producer and the consumer that share a common fixed-size
+// buffer and use it as a queue.
+//  The producer’s job is to generate data, put it into the buffer, and start again.
+//  At the same time, the consumer is consuming the data (i.e., removing it from the buffer),
+// one piece at a time.
+// What is the Actual Problem?
+// Given the common fixed-size buffer, the task is to make sure that the producer can’t add data
+// into the buffer when it is full and the consumer can’t remove data from an empty buffer.
+// Accessing memory buffers should not be allowed to producers and consumers at the same time.
+// Solution of Producer-Consumer Problem
+// The producer is to either go to sleep or discard data if the buffer is full. The next time the
+// consumer removes an item from the buffer, it notifies the producer, who starts to fill the buffer
+// again. In the same manner, the consumer can go to sleep if it finds the buffer to be empty. The
+// next time the producer transfer data into the buffer, it wakes up the sleeping consumer.
